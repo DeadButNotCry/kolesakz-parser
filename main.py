ba3a62ee7171520f6ed247ecfdb23bc565dcbb4c
BoysNeverCry <deadbutnotcry@gmail.com>
2022-07-13 23:06:51 +0300

    mb end

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 from email import message
+import math
+from time import sleep
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -9,11 +11,12 @@ list = [""]
 
 
 def send_to_tg(text):
-    send_text = 'https://api.telegram.org/bot' + sets.bot_token + '/sendMessage?chat_id=' + sets.bot_chatID + '&parse_mode=HTML&text=' + text
+    send_text = 'https://api.telegram.org/bot' + sets.bot_token + \
+        '/sendMessage?chat_id=' + sets.bot_chatID + '&parse_mode=HTML&text=' + text
     requests.get(send_text)
 
 
-def main(city):
+def main(city, user_percent):
     text = requests.get("https://m.kolesa.kz/hot/cars/").text
     bs = BeautifulSoup(text, "lxml")
     cars = bs.find_all(class_="search-list__item")
@@ -22,29 +25,47 @@ def main(city):
     if (a is not None) and (list[0] != a["href"]):
         href = a["href"]
         message_text = pattern+href+"\n"
+        print(pattern+href)
         list[0] = href
         # TODO: add city check
         region = a.find(class_="a-card-info__region").text.strip().lower()
-        car_text = requests.get(pattern+href).text
-        car_bs = BeautifulSoup(car_text, "lxml")
+        if city.lower() in region:  # city.lower() in region
+            car_text = requests.get(pattern+href).text
+            car_bs = BeautifulSoup(car_text, "lxml")
 
-        properties_div = car_bs.find(class_="a-block a-properties")
-        properties_list = properties_div.find_all(class_="a-properties__info")
-        for i in properties_list:
-            values = i.find_all("div")
-            message_text += values[0].text.strip()+" " + \
-                values[1].text.strip()+"\n"
+            properties_div = car_bs.find(class_="a-block a-properties")
+            properties_list = properties_div.find_all(
+                class_="a-properties__info")
+            for i in properties_list:
+                values = i.find_all("div")
+                message_text += "<b>"+values[0].text.strip()+"</b> " + \
+                    values[1].text.strip()+"\n"
 
-        if "Растаможен в Казахстане Да" in message_text:
-            notes = car_bs.find(class_="a-block a-notes").find_all("p")
-            for i in notes:
-                text_note = re.sub(" +", " ", i.text).replace("\n", "")
-                message_text += text_note+"\n"
-            price_info = requests.get(
-                "https://m.kolesa.kz/a/average-price/"+href.split("/")[-1]).json()
-            message_text = price_info["data"]["name"] + "\n"+message_text + "\n" + "Разница в процентах: " + str(price_info["data"]["diffInPercents"])
-            send_to_tg(message_text)
+            if "<b>Растаможен в Казахстане</b> Да" in message_text:
+                notes_block = car_bs.find(class_="a-block a-notes")
+                if notes_block is not None:
+                    notes = notes_block.find_all("p")
+                for i in notes:
+                    text_note = re.sub(" +", " ", i.text).replace("\n", "")
+                    message_text += text_note+"\n"
+                price_info = requests.get(
+                    "https://m.kolesa.kz/a/average-price/"+href.split("/")[-1]).json()
+                if not "error_code" in price_info:
+                    message_text = "<b>" + \
+                        price_info["data"]["name"] + \
+                        "</b>" + "\n"+message_text + "\n"
+
+                    percent = price_info["data"]["diffInPercents"]
+                    if percent <= 0 and abs(percent) >= user_percent:
+                        message_text += "<b> Дешевле на " + str(price_info["data"]["averagePrice"]-price_info["data"]
+                                                                ["currentPrice"]) + " T или на " + str(abs(percent)) + "% </b>"
+                        send_to_tg(message_text)
 
 
 if __name__ == "__main__":
-    main("check")
+    city = input("Введите город: ")
+    percent = float(input("На сколько процентов машина должна быть дешевле: "))
+    while True:
+        main(city, percent)
+        sleep(10)
+        print("parsing")
--- sets.py
+++ sets.py
@@ -1,3 +1,5 @@
-bot_token = '1722258728:AAERPmH07XyUs-ovAkBo72vEvZ6GIB9RyeE' #в этих скобочках нужно ввести токен вашего тг бота
+bot_token = '5592773103:AAFT2mR7fdkdSS_pXGLTrM_5aiHkrSWUj5s' #в этих скобочках нужно ввести токен вашего тг бота
 
-bot_chatID = '415604189'
+bot_chatID = '1655037582'  
+#1655037582
+#415604189
